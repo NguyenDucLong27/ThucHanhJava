43fcf8f04c748aad09125b666e444e361ea38fbf
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-02-15 15:25:07 +0700

    Create app55.java

---
+++ BaiThucHanh1502/javaapplication10/app55.java
@@ -0,0 +1,13 @@
+
+package javaapplication10;
+public class app55 {
+public static void main(String[] args){
+    System.out.print("Cac so chia het cho 5 la");
+    for(int i=0;i<20;i++){
+        if (i%5 !=0){
+            continue;
+        }
+     System.out.print(i);
+    }        
+}    
+}
