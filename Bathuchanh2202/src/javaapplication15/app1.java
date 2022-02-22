45f185b835113aafeeab2662d7f9cfceede902a5
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-02-22 15:08:57 +0700

    Create app1.java

---
+++ Bathuchanh2202/src/javaapplication15/app1.java
@@ -0,0 +1,21 @@
+﻿package javaapplication15;
+import java.util.Scanner;        
+public class app1{ 
+public static void main(String[] args) {
+int so1, so2;
+Scanner scanner = new Scanner(System.in);
+System.out.print("Nhập số thứ nhất:");
+so1 = (int)scanner.nextInt();
+System.out.print("Nhập số thứ hai:");
+so2 = (int)scanner.nextInt();
+scanner.close();
+while (so1 != so2) {
+if(so1 > so2)
+so1 = so1 - so2;
+else
+so2 = so2 - so1;
+}
+System.out.printf("Ước chung lớn nhất là: %d", so2);
+}
+} 
+
