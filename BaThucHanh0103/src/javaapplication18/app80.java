8c57567dcdc27d431967323d9175b1e8ae8b2a52
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-03-01 14:16:49 +0700

    Create app80.java

---
+++ BaThucHanh0103/src/javaapplication18/app80.java
@@ -0,0 +1,29 @@
+package javaapplication18;
+import java.util.Scanner;
+public class app80 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int sodong,socot;
+    System.out.print("Nhap vao so dong cua ma tran :");
+    sodong=sc.nextInt();
+    System.out.print("Nhap vao so cot cua ma tran :");
+    socot=sc.nextInt();
+    int [][]mt= new int[sodong][socot];
+    System.out.println("Nhap vao cac phan tu cua ma tran :");
+    for(int i=0;i<sodong;i++)
+        for(int j=0;j<socot;j++)
+                {
+                System.out.print("mt[" +i+ "]["+j+"]=");
+                mt[i][j]=sc.nextInt();
+                }
+    int max=mt[0][0];
+    for (int i=0;i<sodong;i++)
+        for(int j=0;i<socot;j++)
+        {
+            if(max<mt[i][j]){
+                max=mt[i][j];
+            }
+        }
+    System.out.println("Phan tu lon nhat cua ma tran  la :" + max);
+    }    
+}
