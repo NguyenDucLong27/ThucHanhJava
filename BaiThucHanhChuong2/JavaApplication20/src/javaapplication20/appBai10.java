6073f96e7fa7fa2754416357889923f9fcc64270
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-03-03 19:30:27 +0700

    Create appBai10.java

---
+++ BaiThucHanhChuong2/JavaApplication20/src/javaapplication20/appBai10.java
@@ -0,0 +1,27 @@
+package javaapplication20;
+import java.util.Scanner;
+public class appBai10 {
+public static void main(String[] args) {
+Scanner sc =new Scanner(System.in);
+String chuoi;
+char kitu;
+int dem=0;
+do 
+{
+    System.out.print("Nhap vao mot chuoi bat ky : ");
+    chuoi=sc.nextLine();
+}
+while(chuoi.length()>80);
+System.out.print("\nNhap vao mot ki tu : ");
+kitu=sc.next().charAt(0);
+char a[]=chuoi.toCharArray();
+for(int i=0;i<a.length;i++)
+{
+    if(a[i]==kitu)
+     {
+       dem++;
+     }
+}
+System.out.print("\nCo "+dem+" ki tu "+kitu+" trong chuoi ");
+}    
+}
