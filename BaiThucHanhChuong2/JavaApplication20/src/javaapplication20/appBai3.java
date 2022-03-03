de4dec118a4f7b90d08793d4fc30a547497de43a
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-03-03 19:30:52 +0700

    Create appBai3.java

---
+++ BaiThucHanhChuong2/JavaApplication20/src/javaapplication20/appBai3.java
@@ -0,0 +1,23 @@
+package javaapplication20;
+import java.util.Scanner;
+public class appBai3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int namsinh,tuoi;
+        System.out.print("Nhap ten : ");
+        String ten = sc.nextLine();
+        System.out.print("Nhap nam : ");
+        namsinh = sc.nextInt();
+        tuoi=2022-namsinh;
+            if(tuoi<16){
+                System.out.print("\nBan " + ten + " dang o do tuoi vi thanh nien");
+            }
+            else if(tuoi>=16 && tuoi<18){
+                System.out.print("\nBan " + ten + " dang o do tuoi truong thanh");
+            }
+            else
+                System.out.print("\nban " + ten + " da gia");
+        
+    }    
+}
+
