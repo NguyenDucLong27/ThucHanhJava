3ee051897b9b9fa61bc9ea2a2a3558cc1ada82e4
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:54 +0700

    Create taodo.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/taodo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class taodo {
+    private int x;
+    private int y;
+    public int getX(){
+        return x;
+    }
+    public void setX(int x){
+        this.x=x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void sety(int y){
+        this.y=y;
+    }    
+}
