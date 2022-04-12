2b93c3e50b5a5618249aa2e3520f19d91a26f78a
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:22 +0700

    Create hinhchunhat75.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/hinhchunhat75.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class hinhchunhat75 extends hinhhoc{
+    public hinhchunhat75(){
+        super();
+    }  
+}
+    
