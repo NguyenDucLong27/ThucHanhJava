caad001881163df0c00e0e40e0dde6e342f9e1b2
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:24 +0700

    Create hinhhoc.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/hinhhoc.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class hinhhoc {
+    public static int dem =0;
+    public hinhhoc(){
+        dem++;
+    }
+    
+}
