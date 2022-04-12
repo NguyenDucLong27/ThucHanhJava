6335e83205aeaf09fda9c0e25175ae7f998e33c7
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:43:23 +0700

    Create hinhhoc.java

---
+++ BAiThucHanh1204/JavaApplication24/src/newpackage/hinhhoc.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author TC
+ */
+public class hinhhoc {
+    public final float pi= 3.14f;
+    public String ten;
+    public float chuvi;
+    public float dientich;
+    public float thetich;
+    public void xuatten(){
+        System.out.print("\n" + ten);
+    }
+    public void inchuvi(){
+        System.out.print("\nChu vi = " + chuvi);
+    }
+    public void indientich(){
+        System.out.print("\nDien tich = " + dientich);
+    }
+    public void inthetich(){
+        System.out.print("\nThe tich = " + thetich);
+    }
+}
