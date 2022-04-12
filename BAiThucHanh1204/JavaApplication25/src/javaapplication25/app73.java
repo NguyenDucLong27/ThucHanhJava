5c6cbd2cdf713630b28d8a89c1e26d6ff1d45a03
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:05 +0700

    Create app73.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/app73.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class app73 {
+    public static void main(String[] args) {
+        toado73 td1= new toado73();
+        td1.x=10;
+        td1.y=20;
+        
+        toado73 td2 = new toado73();
+        td2.x=5;
+        td2.y=6;
+        
+        toado73.thongtin= "Luu toa do hinh hoc";
+    }
+}
