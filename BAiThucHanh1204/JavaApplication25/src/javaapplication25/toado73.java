10aaffd5726fb56e73f3363715e2cea525ba64ac
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:57 +0700

    Create toado73.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/toado73.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class toado73 {
+   public static String thongtin;
+   public int x,y;
+}
