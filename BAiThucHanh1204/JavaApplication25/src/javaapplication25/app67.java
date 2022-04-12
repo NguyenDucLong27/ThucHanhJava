772119dc035afe24aa585b0c787ef1dbc39a75d3
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:45:57 +0700

    Create app67.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/app67.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class app67 {
+    public static void main(String[] args) {
+        hinhtron ht=new hinhtron();
+        ht.xuatthongtin();
+        
+        hinhtru htru=new hinhtru();
+        htru.xuatthongtin();
+    }
+}
