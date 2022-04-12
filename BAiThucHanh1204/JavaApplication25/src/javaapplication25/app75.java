64b9953c3d15b84e6be624b0708955edb8f90266
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:07 +0700

    Create app75.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/app75.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class app75 {
+    public static void main(String[] args) {
+        hinhhoc hh=new hinhhoc();
+        hinhtron75 ht1 = new hinhtron75();
+        hinhtron75 ht2 = new hinhtron75();
+        hinhchunhat75 hcn = new hinhchunhat75();
+        
+        System.out.println("Co tat ca " + hinhhoc.dem + "hinh trong ung dung");
+    }
+    
+}
