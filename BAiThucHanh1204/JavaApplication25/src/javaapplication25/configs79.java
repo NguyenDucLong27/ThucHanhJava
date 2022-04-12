2c593a3b41acdc73a650762812845f38c1370581
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:16 +0700

    Create configs79.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/configs79.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class configs79 {
+    public static final int nv_sep=1,nv_linh=2;
+    public static final long luong_sep=20000000,luong_nvfulltimelinh=10000000,luong_nvparttimengay=800000,luong_nvparttimegio=100000;
+    public static int loaiChucVu = nv_linh;
+}
