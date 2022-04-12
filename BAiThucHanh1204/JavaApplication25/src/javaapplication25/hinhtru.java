4cd161ac9a8958542b6cc8885c1493b3d6864ae3
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-12 19:46:39 +0700

    Create hinhtru.java

---
+++ BAiThucHanh1204/JavaApplication25/src/javaapplication25/hinhtru.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+/**
+ *
+ * @author TC
+ */
+public class hinhtru extends hinhtron{
+    @Override
+    public void xuatthongtin(){
+        System.out.print("\nDay la hinh tru");
+    }
+}
