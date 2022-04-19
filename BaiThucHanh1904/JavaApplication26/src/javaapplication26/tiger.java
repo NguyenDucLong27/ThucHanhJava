7cb1b310c60df35fadfb0660349c2b6f994df260
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-19 15:47:33 +0700

    Create tiger.java

---
+++ BaiThucHanh1904/JavaApplication26/src/javaapplication26/tiger.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication26;
+
+/**
+ *
+ * @author TC
+ */
+ class tiger extends animal {
+    void eat(){
+      System.out.println("Thức ăn là : hươu ,nai, lợn rừng,...");  
+    }
+    void sleep(){
+      System.out.println("Hoạt động ban ngày,ngủ ban đêm.");         
+    }
+    void walk(){
+      System.out.println("Tốc độ đi bộ : ??");         
+    }
+    void run(){
+      System.out.println("Tốc độ chạy : 49-65km");         
+    }
+    void roar(){
+      System.out.println("Tiếng gầm : lớn ,to,vang");         
+    }
+}
