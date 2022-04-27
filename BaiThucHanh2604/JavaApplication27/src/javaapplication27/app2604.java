70f7f7ced30aca97cd3ef3228257cecb55bbb685
NguyenDucLong27 <97503684+NguyenDucLong27@users.noreply.github.com>
2022-04-27 16:17:21 +0700

    Create app2604.java

---
+++ BaiThucHanh2604/JavaApplication27/src/javaapplication27/app2604.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication27;
+
+/**
+ *
+ * @author TC
+ */
+public class app2604 {
+    public static void main(String[] args) {
+        point d1=new point();
+        d1.nhapdiem();
+        d1.indiem();
+        
+        tamgiac tg1=new tamgiac();
+        tg1.nhap();
+        tg1.check();
+    }
+    
+}
+    
+
